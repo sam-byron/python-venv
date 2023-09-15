a9956f9683832d2562ed2abd4a950f6dae6ba3f9
Sam Byron <mrc4090@proton.me>
2023-09-14 19:37:28 -0700

    Print root folder of all venvs on computer

---
+++ venv_list_xargs.zsh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#References:
+#https://www.baeldung.com/linux/loop-over-find-bad-practice
+
+all_venvs=$(find ~  -name "site-packages" -print0 | xargs -0 ./venv_list_helper.zsh)
+echo "Choose a python virtual environment to activate:"
+echo $all_venvs
