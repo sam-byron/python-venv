804e3e488a0ed9e17eacc9f45aee9457544b46c6
Sam Byron <mrc4090@proton.me>
2023-09-14 20:33:18 -0700

    Print venv options for user selection

--- venv_list_xargs.zsh
+++ venv_list_xargs.zsh
@@ -1,7 +1,17 @@
 #!/bin/bash
 #References:
 #https://www.baeldung.com/linux/loop-over-find-bad-practice
+#https://www.golinuxcloud.com/bash-split-string-into-array-linux/
+#https://linuxize.com/post/bash-increment-decrement-variable/
 
-all_venvs=$(find ~  -name "site-packages" -print0 | xargs -0 -n 1 ./venv_list_helper.zsh)
+out=$(find ~  -name "site-packages" -print0 | xargs -0 -n 1 ./venv_list_helper.zsh)
 echo "Choose a python virtual environment to activate:"
-echo $all_venvs
+all_venvs=($out)
+
+i=1
+for venv in "${all_venvs[@]}"
+do
+    echo "$i" "$venv"
+    let "i+=1"
+done
+
