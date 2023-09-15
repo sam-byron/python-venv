1d89bec94001a7023af2d3af46bff82926accc1d
Sam Byron <mrc4090@proton.me>
2023-09-14 23:07:11 -0700

    Tracking venv alias'

---
+++ venv_cmds.zsh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Python virtual environments
+alias venv_create="python3 -m venv"
+alias venv_list="source ~/.oh-my-zsh/custom/venv/venv_list_xargs.zsh"
+alias venv_activate=". ./bin/activate"
+alias venv_deactivate="deactivate"
