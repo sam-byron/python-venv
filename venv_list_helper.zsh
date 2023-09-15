217bbfe94f6d1c82ce1854f269073ea400556d66
Sam Byron <mrc4090@proton.me>
2023-09-14 19:48:35 -0700

    Make code simpler by passing limiting xargs to one input at a time

--- venv_list_helper.zsh
+++ venv_list_helper.zsh
@@ -3,13 +3,8 @@
 #https://medium.com/@shigmas/path-manipulation-in-the-shell-6f02dee65098
 #https://opensource.com/article/18/5/you-dont-know-bash-intro-bash-arrays
 
-all_venvs=()
-for dir in "$@"; do
-	venv_root="${dir%*/*}"
-	venv_root="${venv_root%*/*}"
-	venv_root="${venv_root%*/*}"
-	# echo $venv_root
-	all_venvs+=( $venv_root )
-done
-
-echo ${all_venvs[@]}
+dir=$@
+venv_root="${dir%*/*}"
+venv_root="${venv_root%*/*}"
+venv_root="${venv_root%*/*}"
+echo $venv_root
--- venv_list_xargs.zsh
+++ venv_list_xargs.zsh
@@ -2,6 +2,6 @@
 #References:
 #https://www.baeldung.com/linux/loop-over-find-bad-practice
 
-all_venvs=$(find ~  -name "site-packages" -print0 | xargs -0 ./venv_list_helper.zsh)
+all_venvs=$(find ~  -name "site-packages" -print0 | xargs -0 -n 1 ./venv_list_helper.zsh)
 echo "Choose a python virtual environment to activate:"
 echo $all_venvs
